80e52ddda8d260428fba962fcc6fdec323f05732
Dave Simons <dave@inuits.be>
2011-08-02 13:05:17 +0200

    provide some interoperability with other modules
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,18 @@
-class passenger {
+class passenger (
+	$webserver = 'httpd'
+) {
   # just in case apache is not included.
 
   # Package is avail be only for Red Hat CentOS 5 
   package{"rubygem-passenger": 
     ensure => "2.2.2-1",
-    require => Package["httpd"],
-    before => Service["httpd"],
+    require => Package["$webserver"],
+    before => Service["$webserver"],
   }
 
   file{"/etc/httpd/conf.d/passenger.conf":
     source => "puppet:///modules/passenger/passenger.conf",
     mode => 644, owner=> root, group => root,
-    notify => Service ["httpd"],
+    notify => Service ["$webserver"],
   }
 }
