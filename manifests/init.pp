52e720523c3ed8fa313e6093309d169dedbada5d
Dave Simons <dave@inuits.be>
2011-08-02 13:07:58 +0200

    cleanup of syntax
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,20 @@
 class passenger (
 	$webserver = 'httpd'
 ) {
-  # just in case apache is not included.
-
-  # Package is avail be only for Red Hat CentOS 5 
-  package{"rubygem-passenger": 
-    ensure => "2.2.2-1",
-    require => Package["$webserver"],
-    before => Service["$webserver"],
+  # Package is available only for RedHat and CentOS 5 
+  package {
+    "rubygem-passenger": 
+       ensure => "2.2.2-1",
+       require => Package["$webserver"],
+       before => Service["$webserver"],
   }
 
-  file{"/etc/httpd/conf.d/passenger.conf":
-    source => "puppet:///modules/passenger/passenger.conf",
-    mode => 644, owner=> root, group => root,
-    notify => Service ["$webserver"],
+  file {
+    "/etc/httpd/conf.d/passenger.conf":
+      source => "puppet:///modules/passenger/passenger.conf",
+      mode => 644,
+      owner=> root,
+      group => root,
+      notify => Service ["$webserver"],
   }
 }
